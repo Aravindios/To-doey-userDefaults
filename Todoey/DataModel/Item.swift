253ef7d4cd6ae6f804a7b1a67aac75e754cb7d18
aravindiOSdev <aravindk.techzar@gmail.com>
2020-04-07 21:23:28 +0530

    to-do-list using userDefults

---
+++ Todoey/DataModel/Item.swift
@@ -0,0 +1,13 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by apple on 06/04/20.
+//  Copyright © 2020 App Brewery. All rights reserved.
+//
+
+import Foundation
+class Item: Codable{
+    var title: String = ""
+    var done: Bool = false
+}
